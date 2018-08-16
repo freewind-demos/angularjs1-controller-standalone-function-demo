911e5adbaa4ed2cdeaf032ce115ec588896a9d83
freewind <nowindlee@gmail.com>
2018-08-16 17:42:28 +0800

    function as controller

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const app = angular.module('app', []);
-app.controller('HelloController', function ($scope) {
+app.controller('HelloController', HelloController);
+
+function HelloController($scope) {
     $scope.userName = "AngularJs1";
     $scope.showHelloWords = false;
     $scope.sayHello = function () {
         this.showHelloWords = true
     }
-});
+}
+
+HelloController.$inject = ['$scope'];
